191993bda941b2c33ecc7bb68bb6ca5c706d8df3
felix.miranda <felixmiranda.net@gmail.com>
2019-07-30 09:53:53 -0500

    Cambio view con PCC

--- DataBase/Views/YESCLOSEDBUYOUT_VIEW.sql
+++ DataBase/Views/YESCLOSEDBUYOUT_VIEW.sql
@@ -1,12 +1,11 @@
 
-CREATE VIEW [dbo].[YesClosedBuyout_View]
+CREATE VIEW [dbo].[YESCLOSEDBUYOUT_VIEW]
 
 AS
 
 SELECT DISTINCT vi.ID as ID, 'VendorInvoices' as Source, vi.BuyoutID, x.AccountID
 , x.AcctControlRef, bo.WorkorderID, isnull(x.WorkorderMod,bo.WorkorderMod) as WorkorderMod
 , x.CostCode as OrderCostCode
---, bt.CategoryNumber
 , case x.AcctControlRef
 	when 'DL' then '010'
 	when 'DE' then '020'
@@ -19,8 +18,6 @@ SELECT DISTINCT vi.ID as ID, 'VendorInvoices' as Source, vi.BuyoutID, x.AccountI
 , convert(DECIMAL(19,8),0) as glPaidAmount
 , x.glStatus
 , CASE       
-	--WHEN vi.VendorInvoiceStatus IN ('Committed','Executed','Closed') AND isnull(vi.InvoiceApprovedAmount,0) <> 0
-	--THEN convert(DECIMAL(19,8),isnull(x.Amount,0) / isnull(vi.InvoiceApprovedAmount,0) * vi.ChangeInRetainage)
 	WHEN vi.VendorInvoiceStatus IN ('Committed','Executed','Closed') AND isnull(vi.ChangeInRetainage,0) <> 0
 	THEN convert(DECIMAL(19,8),vi.ChangeInRetainage)
 	ELSE convert(DECIMAL(19,8),0)      
@@ -31,27 +28,17 @@ SELECT DISTINCT vi.ID as ID, 'VendorInvoices' as Source, vi.BuyoutID, x.AccountI
 , bo.Ordnum
 FROM VendorInvoices vi         
 INNER JOIN Buyout bo on bo.BuyoutID = vi.BuyoutID   
---INNER JOIN BuyoutLines bl on bl.Recnum = bo.BuyoutID     
---INNER JOIN BuyoutType bt on bo.BoTypeID = bt.BoTypeID
 LEFT JOIN (          
-	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, jc.CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
-	FROM JobCost jc                     
+	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, ISNULL(pcc.CostCodeNumber, jc.CostCode) CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
+	FROM JobCost jc     
+	LEFT JOIN dbo.ProjectCostCodes pcc	ON pcc.ProjectCostCodeID = jc.ProjectCostCodeID and pcc.status = 'on'      	
 	INNER JOIN GeneralLedger gl on jc.GeneralLedgerID = gl.GeneralLedgerID           
 	INNER JOIN GeneralLedgerLines gll on jc.GeneralLedgerLineID = gll.UniqueID
 	INNER JOIN AccountingControl ac on ac.AcctControlRef = gll.AcctControlRef
 	WHERE gl.Status <> 'Cancelled'
 ) x on x.SourceID = vi.ID
 	and x.Source = 'VendorInvoices'
-	--and x.CostCode = bl.Cstcde
 	and x.WorkorderID = bo.WorkorderID
-	/*
-		and x.AcctControlRef = (case bt.CategoryNumber
-		when '010' then 'DL'
-		when '020' then 'DE'
-		when '030' then 'DM'
-		when '040' then 'DS'
-		end)
-	*/
 WHERE bo.Status IN ('Committed','Executed','Closed')        
 AND vi.VendorInvoiceStatus IN ('Committed','Executed','Closed')
 AND NOT bo.BoTypeID IN (3,4,5)        
@@ -61,7 +48,6 @@ UNION ALL
 SELECT DISTINCT tsl.TSlineID as ID, 'Timesheets' as Source, tsl.BuyoutID, x.AccountID
 , x.AcctControlRef, bo.WorkorderID, isnull(x.WorkorderMod,bo.WorkorderMod) as WorkorderMod
 , x.CostCode as OrderCostCode
---, bt.CategoryNumber
 , case x.AcctControlRef
 	when 'DL' then '010'
 	when 'DE' then '020'
@@ -81,27 +67,18 @@ SELECT DISTINCT tsl.TSlineID as ID, 'Timesheets' as Source, tsl.BuyoutID, x.Acco
 FROM TimesheetLines tsl
 INNER JOIN Timesheet ts on ts.TSid = tsl.TSid
 INNER JOIN Buyout bo on bo.BuyoutID = tsl.BuyoutID
---INNER JOIN BuyoutLines bl on bl.Recnum = bo.BuyoutID
---INNER JOIN BuyoutType bt on bo.BoTypeID = bt.BoTypeID
 LEFT JOIN (          
-	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, jc.CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
-	FROM JobCost jc                     
+	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source,  ISNULL(pcc.CostCodeNumber, jc.CostCode) CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
+	FROM JobCost jc 
+	LEFT JOIN dbo.ProjectCostCodes pcc	ON pcc.ProjectCostCodeID = jc.ProjectCostCodeID and pcc.status = 'on'      		
 	INNER JOIN GeneralLedger gl on jc.GeneralLedgerID = gl.GeneralLedgerID           
 	INNER JOIN GeneralLedgerLines gll on jc.GeneralLedgerLineID = gll.UniqueID
 	INNER JOIN AccountingControl ac on ac.AcctControlRef = gll.AcctControlRef
 	WHERE gl.Status <> 'Cancelled'
 ) x on x.SourceID = tsl.TSlineID
 	and x.Source = 'Timesheets'
-	--and x.CostCode = bl.Cstcde
 	and x.WorkorderID = bo.WorkorderID
-	/*
-		and x.AcctControlRef = (case bt.CategoryNumber
-		when '010' then 'DL'
-		when '020' then 'DE'
-		when '030' then 'DM'
-		when '040' then 'DS'
-		end)
-	*/
+
 WHERE bo.Status IN ('Committed','Executed','Closed')        
 AND bo.BoTypeID = 3        
     
@@ -110,7 +87,6 @@ UNION ALL
 SELECT DISTINCT erl.ERLinesID as ID, 'Equipment' as Source, erl.BuyoutID, x.AccountID
 , x.AcctControlRef, bo.WorkorderID, isnull(x.WorkorderMod,bo.WorkorderMod) as WorkorderMod
 , x.CostCode as OrderCostCode
---, bt.CategoryNumber
 , case x.AcctControlRef
 	when 'DL' then '010'
 	when 'DE' then '020'
@@ -129,27 +105,17 @@ SELECT DISTINCT erl.ERLinesID as ID, 'Equipment' as Source, erl.BuyoutID, x.Acco
 , bo.Ordnum
 FROM ERLines erl         
 INNER JOIN Buyout bo on bo.BuyoutID = erl.BuyoutID
---INNER JOIN BuyoutLines bl on bl.Recnum = bo.BuyoutID
---INNER JOIN BuyoutType bt on bo.BoTypeID = bt.BoTypeID
 LEFT JOIN (          
-	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, jc.CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
-	FROM JobCost jc                     
+	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, ISNULL(pcc.CostCodeNumber, jc.CostCode) CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
+	FROM JobCost jc            
+	LEFT JOIN dbo.ProjectCostCodes pcc	ON pcc.ProjectCostCodeID = jc.ProjectCostCodeID and pcc.status = 'on'      			
 	INNER JOIN GeneralLedger gl on jc.GeneralLedgerID = gl.GeneralLedgerID           
 	INNER JOIN GeneralLedgerLines gll on jc.GeneralLedgerLineID = gll.UniqueID
 	INNER JOIN AccountingControl ac on ac.AcctControlRef = gll.AcctControlRef
 	WHERE gl.Status <> 'Cancelled'
 ) x on x.SourceID = erl.ERLinesID
 	and x.Source = 'Equipment'
-	--and x.CostCode = bl.Cstcde
 	and x.WorkorderID = bo.WorkorderID
-	/*
-		and x.AcctControlRef = (case bt.CategoryNumber
-		when '010' then 'DL'
-		when '020' then 'DE'
-		when '030' then 'DM'
-		when '040' then 'DS'
-		end)
-	*/
 WHERE bo.Status IN ('Committed','Executed','Closed')        
 AND bo.BoTypeID = 4        
     
@@ -158,7 +124,6 @@ UNION ALL
 SELECT DISTINCT e.ExpenseUniqueID as ID, 'ExpenseReimbursement' as Source, e.BuyoutID, x.AccountID
 , x.AcctControlRef, bo.WorkorderID, isnull(x.WorkorderMod,bo.WorkorderMod) as WorkorderMod
 , x.CostCode as OrderCostCode
---, bt.CategoryNumber
 , case x.AcctControlRef
 	when 'DL' then '010'
 	when 'DE' then '020'
@@ -177,29 +142,19 @@ SELECT DISTINCT e.ExpenseUniqueID as ID, 'ExpenseReimbursement' as Source, e.Buy
 , bo.Ordnum
 FROM Expenses e        
 INNER JOIN Buyout bo on bo.BuyoutID = e.BuyoutID
---INNER JOIN BuyoutLines bl on bl.Recnum = bo.BuyoutID
---INNER JOIN BuyoutType bt on bo.BoTypeID = bt.BoTypeID
 LEFT JOIN (          
-	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, jc.CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
-	FROM JobCost jc                     
+	SELECT gl.GeneralLedgerID, jc.GeneralLedgerLineID, gl.Status as glStatus, gll.Amount, gl.SourceID, gl.Source, ISNULL(pcc.CostCodeNumber, jc.CostCode) CostCode, jc.WorkorderID, jc.WorkorderMod, ac.AccountID, gll.AcctControlRef          
+	FROM JobCost jc  
+	LEFT JOIN dbo.ProjectCostCodes pcc	ON pcc.ProjectCostCodeID = jc.ProjectCostCodeID and pcc.status = 'on'      				
 	INNER JOIN GeneralLedger gl on jc.GeneralLedgerID = gl.GeneralLedgerID           
 	INNER JOIN GeneralLedgerLines gll on jc.GeneralLedgerLineID = gll.UniqueID
 	INNER JOIN AccountingControl ac on ac.AcctControlRef = gll.AcctControlRef
 	WHERE gl.Status <> 'Cancelled'
 ) x on x.SourceID = e.ExpenseUniqueID
 	and x.Source = 'ExpenseReimbursement'
-	--and x.CostCode = bl.Cstcde
 	and x.WorkorderID = bo.WorkorderID
-	/*
-		and x.AcctControlRef = (case bt.CategoryNumber
-		when '010' then 'DL'
-		when '020' then 'DE'
-		when '030' then 'DM'
-		when '040' then 'DS'
-		end)
-	*/
 WHERE bo.Status IN ('Committed','Executed','Closed')        
 AND bo.BoTypeID = 5 
 
-/**********************************************************************************/
-GO
+
+
